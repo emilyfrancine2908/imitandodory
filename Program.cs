f79eaf37b0e238943161b5e2e491a368a147956c
ETEC Adolpho Berezin <meu-email@meu-servidor.com>
2024-03-15 22:33:32 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.Clear();
+
+ Console.WriteLine("tradutor de baleês \n");
+ Console.Write("Digite o texto a ser traduzido: ");
+
+ string texto = Console.ReadLine()!;
+
+ Console.WriteLine("\nA dory diria...\n");
+
+ Console.WriteLine(texto
+ .Replace("a" , "aaa")
+ .Replace("i" , "iii")
+ .Replace("u" , "uuu")
+ .Replace("e" , "eee")
+ .Replace("o" , "ooo")
+ .Replace("a" , "aaa")
+ .Replace("i" , "iii")
+ .Replace("u" , "uuu")
+ .Replace("e" , "eee")
+ .Replace("o" , "ooo")
+ );
+ Console.WriteLine();
